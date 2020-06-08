c8231cb3c26a97377f4d1f82d2c4b2976c9d6326
Nathan Souffan <souffan.nathan@gmail.com>
2020-06-08 17:10:47 +0200

    Fix the image and add thickness

--- draw.py
+++ draw.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from model import Model
+import numpy as np
 white = (255,255,255)
 black = (0,0,0)
 
@@ -20,8 +21,13 @@ class Grid():
     def update(self, mouse, window_size):
         x = mouse[0] // (window_size // 28)
         y = mouse[1] // (window_size // 28)
-        self.pixels[x][y] = 1
-
+        for i in range(-1, 2):
+            if i+x >= 28 or i+x < 0:
+                continue
+            for j in range(-1, 2):
+                if j+y >= 28 or j+y < 0:
+                    continue
+                self.pixels[i+x][y+j] = 1
 
 def main():
 
@@ -35,7 +41,8 @@ def main():
                 if event.type == pygame.QUIT:
                     running = 0
                 elif event.type == pygame.KEYDOWN and event.key == K_SPACE:
-                    predict = model.predict(grid.pixels)
+                    im = np.array(grid.pixels).T.tolist()
+                    predict = model.predict(im)
                     print(predict)
                     grid = Grid()
                 elif pygame.mouse.get_pressed()[0] == True:
