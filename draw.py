382298f29d900fbc3410e514220a344733ae3bc6
Nathan Souffan <souffan.nathan@gmail.com>
2020-06-09 00:24:21 +0200

    Add grayscale pixels when in draw

--- draw.py
+++ draw.py
@@ -6,6 +6,8 @@ from tkinter import *
 import tkinter.messagebox as msg
 white = (255,255,255)
 black = (0,0,0)
+gray1 = (128,128,128)
+gray2 = (192,192,192)
 
 class Grid():
     def __init__(self):
@@ -16,21 +18,45 @@ class Grid():
         pixel_size = window.get_width() // 28
         for i in range(28):
             for j in range(28):
+                if self.pixels[i][j] == 0:
+                    continue
+                x = i * pixel_size
+                y = j * pixel_size
+                color = None
+                rect = (x, y, pixel_size, pixel_size)
                 if self.pixels[i][j] == 1:
-                    x = i * pixel_size
-                    y = j * pixel_size
-                    pygame.draw.rect(window, black, (x, y, pixel_size, pixel_size))
+                    color = black
+                elif self.pixels[i][j] == 0.5:
+                    color = gray1
+                elif self.pixels[i][j] == 0.25:
+                    color = gray2
+                pygame.draw.rect(window, color, rect)
     def update(self, mouse, window_size):
         x = mouse[0] // (window_size // 28)
         y = mouse[1] // (window_size // 28)
-        for i in range(-1, 2):
-            if i+x >= 28 or i+x < 0:
-                continue
-            for j in range(-1, 2):
-                if j+y >= 28 or j+y < 0:
-                    continue
-                self.pixels[i+x][y+j] = 1
-
+        self.pixels[x][y] = 1
+        self.update_diag(x, y)
+        self.update_orth(x, y)
+        # for i in range(-1, 2):
+            # if i+x >= 28 or i+x < 0:
+                # continue
+            # for j in range(-1, 2):
+                # if j+y >= 28 or j+y < 0:
+                    # continue
+                # self.pixels[i+x][y+j] = 1
+    def in_range(x, y):
+        return x < 28 and y < 28 and x >= 0 and y >=0
+    def update_diag(self, x, y):
+        for i in range(x-1, x+2, 2):
+            for j in range(y-1, y+2, 2):
+                if Grid.in_range(i, j) and self.pixels[i][j] < 0.25:
+                    self.pixels[i][j] = 0.25
+    def update_orth(self, x, y):
+        for i in range(-1, 2, 2):
+            if Grid.in_range(x+i, y) and self.pixels[x+i][y] < 0.5:
+                self.pixels[x+i][y] = 0.5
+            if Grid.in_range(x, y+i) and self.pixels[x][y+i] < 0.5:
+                self.pixels[x][y+i] = 0.5
 def main():
 
     grid = Grid()
